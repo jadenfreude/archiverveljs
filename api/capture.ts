8a5988d304fa6a98947c6aa5ee9671b12d9f858b
Jad <rahman.elzein@gmail.com>
2023-10-31 14:00:37 +0300

    fix

--- api/capture.ts
+++ api/capture.ts
@@ -3,6 +3,6 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name } = req
   return res.json({
-    message: `Hello ${name.name}!`,
+    message: `Hello ${name}!`,
   })
 }
